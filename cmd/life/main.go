7c93ed0083b8ea7a95027a5d15d3e19ffd3517f2
Jeremy Miller <jgmiller88@gmail.com>
2018-03-06 22:15:53 -0800

    fill in main

--- cmd/life/main.go
+++ cmd/life/main.go
@@ -1,1 +1,12 @@
+/*
+Package main executes a certain number of iterations of the game of the life.
+*/
 package main
+
+import "github.com/jeremy-miller/life-go/internal/life"
+
+const iterations = 5
+
+func main() {
+	life.Run(iterations)
+}
