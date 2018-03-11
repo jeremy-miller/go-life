11c15d3d79e781717c50455b2208b854a9368f67
Jeremy Miller <jgmiller88@gmail.com>
2018-03-11 10:36:52 -0700

    update documentation

--- internal/life/game.go
+++ internal/life/game.go
@@ -8,7 +8,7 @@ package life
 import "time"
 
 // Run initializes the game board with an initial configuration, then runs the game the provided
-// number of iterations.  The board state is printed between iterations.
+// number of iterations.  The initial board state is printed, as well as the state between iterations.
 //
 // Currently the only supported initial configuration is "blinker", which is used by default.
 func Run(iterations int) {
